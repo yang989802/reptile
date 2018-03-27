5e208336e3cd98fcc422dc03bfa1c4de54623078
87106 <871067153@qq.com>
2018-03-28 02:12:39 +0800

    test

--- pic_reptile/urlReptile.py
+++ pic_reptile/urlReptile.py
@@ -5,16 +5,7 @@ import urllib
 import time
 import os
 
-def cbk(a,b,c):
-    '''''回调函数
-    @a:已经下载的数据块
-    @b:数据块的大小
-    @c:远程文件的大小
-    '''
-    per=100.0*a*b/c
-    if per>100:
-        per=100
-    print ('%.2f%%' % per)
+
 
 def mkdir(path):
     isExists = os.path.exists(path)
@@ -39,18 +30,25 @@ DETAIL_IMG_URL=[] #表情包详情页
 BASE_PAGE_URL ='https://www.doutula.com/article/list/?page='#主页面获取详情页
 IMG_URL=[]#图片下载地址
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-for x in range(1, 3):
+for x in range(1, 10):
     url = BASE_PAGE_URL + str(x)
     PAGE_URL_LIST.append(url)
 
 for url in PAGE_URL_LIST:
     req = urllib.request.Request(url=url,headers=headers)
-    page = request.urlopen(req).read()
-    page = page.decode('utf-8')
-    soup = BeautifulSoup(page,'lxml')
-    file_path = soup.find_all('a',attrs={'class':'list-group-item random_list'})
-    for link in file_path:
-        DETAIL_IMG_URL.append(link['href'])
+
+    try:
+        page = request.urlopen(req).read()
+        time.sleep(0.5)
+    except:
+        print('error->url'+str(url))
+        continue
+    else:
+        page = page.decode('utf-8')
+        soup = BeautifulSoup(page,'lxml')
+        file_path = soup.find_all('a',attrs={'class':'list-group-item random_list'})
+        for link in file_path:
+            DETAIL_IMG_URL.append(link['href'])
 
 while len(DETAIL_IMG_URL)!=0:
     url=DETAIL_IMG_URL.pop()
@@ -74,12 +72,14 @@ while len(DETAIL_IMG_URL)!=0:
         url = IMG_URL.pop()
         if not (('https' in url) or ('http' in url)):
             url='http:'+url
-        urllib.request.urlretrieve(
-            url,
-            path+str(index)+'.jpg',
-            cbk
-        )
-        index=index+1
+        try:
+            urllib.request.urlretrieve(
+                url,
+                path+str(index)+'.jpg'
+            )
+            index=index+1
+        except:
+            print('error-> '+url+'\n'+'filename:'+path)
 
 
 
